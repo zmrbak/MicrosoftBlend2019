fa7f163120ef2d7d76dbc3897f3ce736a1993a96
zmrbak <20995658@qq.com>
2019-07-30 23:06:04 +0800

    上传代码

---
+++ 课程代码/第40课、CustomControlDemo40/CustomControlDemo40/CustomControlDemo40/UserControlAB.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomControlDemo40
+{
+    /// <summary>
+    /// UserControlAB.xaml 的交互逻辑
+    /// </summary>
+    public partial class UserControlAB : UserControl
+    {
+        public UserControlAB()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("A");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("B");
+        }
+    }
+}
